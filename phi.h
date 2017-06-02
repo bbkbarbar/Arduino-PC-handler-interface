6ba2c2a6253c8c51440266d38fda17823fb1f965
bbkbarbar <bbk.barbar@gmail.com>
2017-06-02 16:52:31 +0200

    .

--- phi.h
+++ phi.h
@@ -4,6 +4,7 @@
 
 #ifndef phi_h
 #define phi_h
+//#include "phi.cpp"
 
 #if defined(ARDUINO) && (ARDUINO >= 100)
 	#include <Arduino.h>
@@ -14,36 +15,82 @@
 #define PC_HANDLER_INTERFACE_LIB_VERSION    "0.0.1"
 
 
-#ifndef PIN_RESET_BUTTON
-	#define PIN_RESET_BUTTON		             4
-#endif
-
-#ifndef PIN_RESET_BUTTON
-	#define PIN_POWER_BUTTON		             5
-#endif
-
-#ifndef PIN_RESET_BUTTON
-	#define PIN_RELAY_1		                     6
-#endif
-#ifndef PIN_RESET_BUTTON
-	#define PIN_RELAY_2		                     7
-#endif
 
 
-#define DEFAULT_BUTTON_SHORT_PRESS_TIME_IN_MS  500
-#define DEFAULT_BUTTON_LONG_PRESS_TIME_IN_MS  6500
 
+class PHI {
 
-class phi {
+	#define AC_STATE_ON                              0
+	#define AC_STATE_OFF                             1
+
+	#define PIN_RESET_BUTTON                         4
+	#define PIN_POWER_BUTTON                         5
+	#define PIN_RELAY_1                              6
+	#define PIN_RELAY_2                              7
+
+	#define DEFAULT_BUTTON_SHORT_PRESS_TIME_IN_MS  500
+	#define DEFAULT_BUTTON_LONG_PRESS_TIME_IN_MS  6500
+	
 	public:
-		void setup();
+		void init();
 	    void pressReset();
 	    void pressReset(long pressing_time_in_ms);
 	    void pressPower();
 	    void pressPower(long pressing_time_in_ms);
 
+	    char turnAC(char newState);
+
 		int temperature;
 
 };
+
+
+char PHI::turnAC(char newState){
+	char previousState1 = digitalRead(PIN_RELAY_1);
+	char previousState2 = digitalRead(PIN_RELAY_2);
+
+	digitalWrite(PIN_RELAY_1, newState);
+	digitalWrite(PIN_RELAY_2, newState);
+
+	if(previousState1 != previousState2){
+		return 3;
+	}
+	return previousState1;
+}
+
+
+void PHI::init() {
+	//mode: INPUT, OUTPUT, or INPUT_PULLUP.
+	pinMode(PIN_RESET_BUTTON, OUTPUT);
+	pinMode(PIN_POWER_BUTTON, OUTPUT);
+	pinMode(PIN_RELAY_1, OUTPUT);
+	pinMode(PIN_RELAY_2, OUTPUT);
+}
+
+
+
+void PHI::pressReset(long pressing_time_in_ms){
+	digitalWrite(PIN_RESET_BUTTON, HIGH);
+	delay(pressing_time_in_ms);
+	digitalWrite(PIN_RESET_BUTTON, LOW);
+}
+
+void PHI::pressReset(){
+	pressReset(DEFAULT_BUTTON_SHORT_PRESS_TIME_IN_MS);
+}
+
+
+
+void PHI::pressPower(long pressing_time_in_ms){
+	digitalWrite(PIN_POWER_BUTTON, HIGH);
+	delay(pressing_time_in_ms);
+	digitalWrite(PIN_POWER_BUTTON, LOW);
+}
+
+void PHI::pressPower(){
+	pressReset(DEFAULT_BUTTON_SHORT_PRESS_TIME_IN_MS);
+}
+
+
 
 #endif
